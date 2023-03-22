c714cfa8b37e6aefcb37f5b2d20100bbed2d754c
arifwidiasan <arifw.subagio17@gmail.com>
2023-03-22 15:18:02 +0700

    add model section

---
+++ model/section.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"time"
+)
+
+type Section struct {
+	ID          int       `gorm:"primaryKey" json:"id"`
+	Title       string    `gorm:"type:varchar(255);not null" json:"title"`
+	Description string    `gorm:"not null" json:"description"`
+	Link        string    `json:"link"`
+	CreatedAt   time.Time `json:"created_at"`
+}
