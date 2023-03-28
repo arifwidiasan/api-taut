205a4d2e24cacba2407a7ae7b16aad1db1ff6ceb
arifwidiasan <arifw.subagio17@gmail.com>
2023-03-28 20:09:38 +0700

    add gitignore and helper create folder

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	conf "github.com/arifwidiasan/api-taut/config"
 	handler "github.com/arifwidiasan/api-taut/controller/handler"
+	"github.com/arifwidiasan/api-taut/helper"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	helper.CreateFolder("../uploads")
+	helper.CreateFolder("../uploads/profile-picture")
 	config := conf.InitConfiguration()
 	e := echo.New()
 
